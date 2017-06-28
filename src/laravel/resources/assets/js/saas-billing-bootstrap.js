dfc2e36f01a976193b548bcfc63cac1152dd99dc
Gilbert Pellegrom <gilbert@pellegrom.me>
2017-06-28 15:59:23 +0100

    Update Laravel integration

--- src/laravel/resources/assets/js/saas-billing-bootstrap.js
+++ src/laravel/resources/assets/js/saas-billing-bootstrap.js
@@ -1,1 +1,6 @@
-Vue.component('billing', require('./components/Billing.vue'));
+import SaaSBilling from 'saas-billing';
+import Billing from './components/Billing.vue';
+
+Vue.use(SaaSBilling);
+
+Vue.component('billing', Billing);
